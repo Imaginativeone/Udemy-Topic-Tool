0c31d3c9d84cb7acf0e51ba2d3b49782ae9510ac
Doug Franklin <doug@dougfranklin.com>
2021-01-03 11:03:29 -0500

    Preview with missing descriptions flagged

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const paidTopics = document.querySelector('#paid-topics');
 const processButton = document.querySelector('#process-topics');
 processButton.addEventListener('click', processTopics);
 
+const listElement = document.querySelector('#fpt-list');
+
 function processTopics() {
   // console.log('Processing Topics - Button Clicked');
 
@@ -18,30 +20,65 @@ function processTopics() {
 
 }
 
+function showTopics(topics) {
+
+  const topicsArray = topics.split("\n");
+
+  topicsArray.map((line, index) => {
+
+    const listItemText = document.createTextNode(`${ line }`);
+    const listItem     = document.createElement('li');
+
+    listItem.appendChild(listItemText);
+    listElement.appendChild(listItem);
+
+  });
+}
+
 function processFreeTopics(topics) {
   
-  console.log('Processing Free Topics', topics);
-  console.log('Move Times to End of Previous Line');
-  
+  // console.log('Processing Free Topics', topics);
+  // console.log('Move Times to End of Previous Line');
   const movedTimes = moveFreeTimes(topics);
 
-  console.log('Account for Preview Topics');
-  console.log('Account for Orphaned Lines'); // No time index or video number
+  // console.log('Account for Preview Topics');
+  const flaggedPreviews = updatePreviews(movedTimes);
+  // console.log('flaggedPreviews', flaggedPreviews);
+
+  showTopics(flaggedPreviews);
+
+  // console.log('Account for Orphaned Lines'); // No time index or video number
+}
+
+function updatePreviews(topics) {
+  const topicsArray = topics.split("\n");
+  // console.log('topicsArray', topicsArray);
+
+  const newArray = topicsArray.map((line) => {
+    const pmd = 'Possible Missing Description ';
+    const flg = '***';
+    if((line.indexOf('Preview') !== -1) && (line.length <= 15)) {
+      return line = pmd + line + flg;
+    } 
+    else {
+      return line;
+    }
+  });
+  // console.log(newArray);
+
+  return newArray.join("\n");
 }
 
 function moveFreeTimes(topics) {
-
-  console.log('Moving Free Times to end of previous line', topics);
-
+  // console.log('Moving Free Times to end of previous line', topics);
   const ftRegex  = /\n([0-9][0-9]:[0-9][0-9])/g;
   const ftResult = topics.replace(ftRegex, ' $1');
-
-  console.log('ftResult', ftResult);
-
+  // console.log('ftResult', ftResult);
+  return ftResult;
 }
 
 function integrateTopics(freeTopics, paidTopics) {
 
-  console.log('Integrating Topics');
+  // console.log('Integrating Topics');
 
 }
