f43d09a557ae9ec4e40338881a9b84b0b0e1fad7
Doug Franklin <doug@dougfranklin.com>
2021-01-03 09:22:23 -0500

    Button Works

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const freeTopics = document.querySelector('#free-topics');
 const paidTopics = document.querySelector('#paid-topics');
 
 const processButton = document.querySelector('#process-topics');
+processButton.addEventListener('click', processTopics);
 
+function processTopics() {
+  console.log('Processing Topics - Button Clicked');
+}
